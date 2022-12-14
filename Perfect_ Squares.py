95d9dccf0a75acc0f8feb85c875291e8f0962da3
Chaitanya Musinoori <91110748+chaitanya326@users.noreply.github.com>
2022-12-14 12:25:48 +0530

    Update Perfect_ Squares.py

--- Perfect_ Squares.py
+++ Perfect_ Squares.py
@@ -1,12 +1,12 @@
 n=int(input('Enter a number: '))         
 def Perfect_Squares(n):
     count=0
-    lists=[]
+    list_of_perfect_squares=[]
     for i in range(n):
         for j in range(i):
             if j*j==i:
-                lists.append(i)
+                list_of_perfect_squares.append(i)
                 count+=1
-    print(f'The numbers of perfect squares between 1 and {n} are {count},and the squares are : {lists}')
+    print(f'The numbers of perfect squares between 1 and {n} are {count},and the squares are : {list_of_perfect_squares}')
         
 Perfect_Squares(n)
